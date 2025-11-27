4ffd1b0bb808f61280e15c1579bfbca631371401
BrandonTay2001 <46275839+BrandonTay2001@users.noreply.github.com>
2025-11-28 00:08:00 +0800

    without gevent

--- backend/gunicorn_config.py
+++ backend/gunicorn_config.py
@@ -1,5 +1,5 @@
 import multiprocessing
 
 bind = "0.0.0.0:5001"
-workers = multiprocessing.cpu_count() * 2 + 1
-worker_class = "gevent"
+workers = multiprocessing.cpu_count() * 2
+# worker_class = "gevent"
